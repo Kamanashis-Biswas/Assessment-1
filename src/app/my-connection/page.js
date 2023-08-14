3c315261762a6501d9e0ba354333aaec5c281182
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-08-15 02:18:01 +0600

    Project Sturecture and route define

---
+++ src/app/my-connection/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "My Connection | Assignment",
+  description: "Next JSF",
+};
+
+const MyConnectionPage = () => {
+  return <div className="mt-5">MyConnectionPage</div>;
+};
+
+export default MyConnectionPage;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import NavLink from "@/components/NavLink";
+
+const navLinks = [
+  {
+    path: "/",
+    title: "My Profile",
+  },
+  {
+    path: "/my-connection",
+    title: "My Connection",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <aside className="mr-10">
+      <h1 className="mt-5 broder border py-2 px-4 rounded-xl mb-10">
+        Dashboard
+      </h1>
+      <ul>
+        {navLinks.map(({ path, title }) => (
+          <li className="my-6" key={path}>
+            <NavLink
+              className="px-3 py-2"
+              exact
+              activeClassName="text-[#1A1558] border border-[#413B89] py-2 px-3 rounded-xl"
+              href={path}
+            >
+              {title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
