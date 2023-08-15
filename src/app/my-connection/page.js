77cef5aadf8985f4abd2c24c50032683808d0505
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-08-16 03:05:31 +0600

    Design Done

--- src/app/my-connection/page.js
+++ src/app/my-connection/page.js
@@ -1,4 +1,6 @@
 import React from "react";
+import avatar from "../../../public/avatar.png";
+import Image from "next/image";
 
 export const metadata = {
   title: "My Connection | Assignment",
@@ -9,10 +11,75 @@ const MyConnectionPage = () => {
   return (
     <div className="mt-28 ">
       <div>
-        <div className="bg-[#1E2875] h-[169px] rounded-lg mr-10">
+        <div className="bg-[#1E2875] h-[85px] md:h-[169px] rounded-lg mx-5">
           <h2 className="text-white text-2xl ml-3 pt-3">My Connection</h2>
         </div>
-        <div></div>
+        <div className="px-5 mt-10">
+          <div className="md:grid grid-cols-3 gap-7">
+            <div className="flex border-slate-400 border-2 p-3 rounded-lg mb-5 justify-between items-center">
+              <div>
+                <h2 className="mb-4">Vishnu Swaroop</h2>
+                <p>Full stack developer</p>
+                <p className="mb-10">@ Oruphones</p>
+                <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                  Remove Connection
+                </button>
+              </div>
+              <div>
+                <Image
+                  className="rounded-full w-[90px] h-[90px]"
+                  src={avatar}
+                  alt="Profile"
+                ></Image>
+              </div>
+            </div>
+            <div className="flex border-slate-400 border-2 p-3 rounded-lg mb-5 justify-between items-center">
+              <div>
+                <h2 className="mb-4">Vishnu Swaroop</h2>
+                <p>Full stack developer</p>
+                <p className="mb-10">@ Oruphones</p>
+                <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                  Remove Connection
+                </button>
+              </div>
+              <div>
+                <Image
+                  className="rounded-full w-[90px] h-[90px]"
+                  src={avatar}
+                  alt="Profile"
+                ></Image>
+              </div>
+            </div>
+          </div>
+          <div className="mt-20">
+            <div>
+              <h2 className="text-xl font-light mb-8">
+                People you can also connect
+              </h2>
+            </div>
+            <div>
+              <div className="md:grid grid-cols-3 gap-7">
+                <div className="flex border-slate-400 border-2 p-3 rounded-lg mb-5 justify-between items-center">
+                  <div>
+                    <h2 className="mb-4">Vishnu Swaroop</h2>
+                    <p>Full stack developer</p>
+                    <p className="mb-10">@ Oruphones</p>
+                    <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                      Remove Connection
+                    </button>
+                  </div>
+                  <div>
+                    <Image
+                      className="rounded-full w-[90px] h-[90px]"
+                      src={avatar}
+                      alt="Profile"
+                    ></Image>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import avatar from "../../public/avatar.png";
+import star from "../../public/star.png";
+import star2 from "../../public/star_2.png";
+import logo from "../../public/logo.png";
 
 const ProfilePage = () => {
   return (
-    <div className="mt-28 ">
+    <div className="mt-28">
       <div>
-        <div className="bg-[#1E2875] h-[85px] md:h-[169px] rounded-lg md:mr-10">
+        <div className="bg-[#1E2875] h-[85px] md:h-[169px] rounded-lg mx-5">
           <h2 className="text-white text-2xl ml-3 pt-3">My Profile</h2>
         </div>
-        <div className="px-5 mt-3">
+        <div className="px-5 mt-10">
           <div className="md:flex gap-10">
             <div className="md:w-1/2">
               <div className="flex justify-between items-center mb-5">
@@ -82,10 +85,109 @@ const ProfilePage = () => {
             </div>
             <div className="md:w-1/2">
               <div>
-                <div>Prof Details</div>
-                <div>Certification</div>
-                <div>Exprence</div>
-                <div>Education</div>
+                <div className="border-slate-400 border-2 p-3 rounded-lg mb-5 flex gap-5 items-center justify-between">
+                  <div>
+                    <h2 className="text-lg font-medium mb-2">
+                      Professional Details
+                    </h2>
+                    <p className="w-2/3">
+                      This are the professional details shown to users in the
+                      app.
+                    </p>
+                  </div>
+                  <div>
+                    <Image
+                      className="w-[48px] h-[48px] mr-10"
+                      src={star}
+                      alt="Star"
+                    ></Image>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between mb-5">
+                    <h2 className="text-lg font-medium mb-2">Certification</h2>
+                    <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                      Edit
+                    </button>
+                  </div>
+                  <div className="border-slate-400 border-2 rounded-full p-5 mb-5">
+                    <div className="flex items-center">
+                      <Image
+                        className="ml-8 md:ml-16"
+                        src={star2}
+                        alt="Star"
+                      ></Image>
+                      <div className="ml-20 md:ml-40 text-center">
+                        <h2>Python</h2>
+                        <p>Coding Ninjas</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <div className="flex items-center justify-between mb-5">
+                      <h2 className="text-lg font-medium mb-2">Experience</h2>
+                      <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                        Edit
+                      </button>
+                    </div>
+                    <div className="border-slate-400 border-2 rounded-lg p-5 mb-5">
+                      <div className="flex justify-between items-center">
+                        <div>
+                          <h2>7 years (2014-2021)</h2>
+                          <p>Oruphones</p>
+                        </div>
+                        <div>
+                          <h2>Full-time</h2>
+                          <p>--Full Stack Developer</p>
+                        </div>
+                        <div>
+                          <Image src={logo} alt="logo"></Image>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="border-slate-400 border-2 rounded-lg p-5 mb-5">
+                      <div className="flex justify-between items-center">
+                        <div>
+                          <h2>6 months (2014)</h2>
+                          <p>Oruphones</p>
+                        </div>
+                        <div>
+                          <h2>Intern</h2>
+                          <p>--Full Stack Developer</p>
+                        </div>
+                        <div>
+                          <Image src={logo} alt="logo"></Image>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between mb-5">
+                    <h2 className="text-lg font-medium mb-2">Education</h2>
+                    <button className="bg-slate-300 px-4 py-1 rounded-2xl">
+                      Edit
+                    </button>
+                  </div>
+                  <div className="border-slate-400 border-2 rounded-lg p-5 mb-5">
+                    <div>
+                      <h2>IIT HYDERABAD</h2>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <h2>(2010-2014)</h2>
+                      <p className="mr-44">Btech</p>
+                    </div>
+                    <div>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur. Erat auctor a
+                        aliquam vel congue luctus. Leo diam cras neque mauris ac
+                        arcu elit ipsum dolor sit amet consectetur.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
